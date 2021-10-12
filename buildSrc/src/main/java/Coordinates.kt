59580d0ddfbdea7ed69e09d61a4c783421ffc191
Abhishek Dubey <abhishekdubey331@gmail.com>
2021-10-12 14:53:00 +0530

    launched

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -3,15 +3,15 @@ const val PUBLISHING_GROUP = "com.technogeeks.statussaver"
 object AppCoordinates {
     const val APP_ID = "com.technogeeks.statussaver.app"
 
-    const val APP_VERSION_NAME = "1.0.0"
-    const val APP_VERSION_CODE = 1
+    const val APP_VERSION_NAME = "1.0.1"
+    const val APP_VERSION_CODE = 2
 }
 
 object LibraryAndroidCoordinates {
-    const val LIBRARY_VERSION = "1.0.0"
-    const val LIBRARY_VERSION_CODE = 1
+    const val LIBRARY_VERSION = "1.0.1"
+    const val LIBRARY_VERSION_CODE = 2
 }
 
 object LibraryKotlinCoordinates {
-    const val LIBRARY_VERSION = "1.0.0"
+    const val LIBRARY_VERSION = "1.0.1"
 }
